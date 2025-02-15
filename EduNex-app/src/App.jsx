48f5a07506853d4695ab6d30544593552c9f8dd1
ridmipoornima <ridmi.20232563@iit.ac.lk>
2025-02-15 15:21:06 +0530

    setup vite

---
+++ EduNex-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./frontend/pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
