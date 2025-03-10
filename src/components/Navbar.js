5a92680d29dc1e63d8e85d835d0f1b6ec595511a
ButhmiraPerera <buthmira.20231968@iit.ac.lk>
2025-03-10 14:40:46 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <nav>
-      
-      <Link to="/admin">Admin Page</Link>
-      <Link to="/student">Student Dashboard</Link>.
-      <Link to="/teacher">Teacher Dashboard</Link>
-      <Link to="/parent">Parent Dashboard</Link>
-    </nav>
-  );
-};
-
-export default Navbar;
