d6eba501dd49b34a015343d58b991f3a5821302b
Panchani Charunya Gunarathne <panchani.20233002@iit.ac.lk>
2025-03-24 00:44:13 +0530

    Delete EduNexapp directory
    Delete EduNexapp directory

--- EduNexapp/src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NotFound() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1>404 - Page Not Found</h1>
-    </div>
-  );
-}
